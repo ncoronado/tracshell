d97d080d8a1b956d1c91be4733d8480fdb758b12
j.kenneth.king <j.kenneth.king@bf24e5f0-f908-11dd-ac5b-dfceb64ead24>
2010-09-18 02:26:50 +0000

    Added empty unit tests... got to get functional test suite from tracrpc working first

---
+++ tests/test_shell.py
@@ -0,0 +1,9 @@
+import unittest
+
+from tracrpc.tests import rpc_testenv
+from tracshell.proxy import TracProxy
+from tracshell.shell import TracShell, ValidationError, CallFailed
+
+
+class ShellTestCase(unittest.TestCase):
+    pass
